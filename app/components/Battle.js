671d26e9281d8fbf4aeddd059d08a783bd816de1
Phanor Coll <phanorcoll@gmail.com>
2017-09-26 20:19:46 -0400

    added the initial structure for the battle result component and the Router path

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var Link = require('react-router-dom').Link;
 
 function PlayerPreview(props) {
     return(
@@ -125,6 +126,7 @@ class Battle extends React.Component{
     }
 
     render(){
+        var match = this.props.match;
         var playerOneName = this.state.playerOneName;
         var playerTwoName = this.state.playerTwoName;
         var playerOneImage = this.state.playerOneImage;
@@ -158,6 +160,13 @@ class Battle extends React.Component{
                             onReset={this.handleReset}
                             id='playerTwo' />}
                 </div>
+
+                {playerOneImage && playerTwoImage &&
+                    <Link className='button' to={{
+                        pathname:match.url + '/results',
+                        search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName
+                    }}>
+                        Battle</Link>}
             </div>
         )
     }
