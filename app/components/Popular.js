8b31f2c225fb101328eb8dd2784f02255d2ae529
Phanor Coll <phanorcoll@gmail.com>
2017-10-04 12:56:59 -0400

    style popular menu
    added more styles and hover effects to the popular menu

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -10,8 +10,8 @@ function SelectLanguage(props){
         <ul className='languages'>
             {languages.map(function(lang){
                 return (
-                    <li style={lang=== props.selectedLanguage ? {color: '#d0021b'}: null} onClick={props.onSelect.bind(null,lang)} key={lang}>
-                        {lang}
+                    <li onClick={props.onSelect.bind(null,lang)} key={lang}>
+                        <a className={lang=== props.selectedLanguage ? 'active-language': null}>{lang}</a>
                     </li>
                 )
             })}
